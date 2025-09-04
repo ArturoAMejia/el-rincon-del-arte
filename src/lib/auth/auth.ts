986e0ad8277b061f995b75d438d506a3ee839a33
Arturo Mejía <armejia02@gmail.com>
2025-09-03 21:02:41 -0600

    fix(lib): delete double prisma instances

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,8 +1,6 @@
-import { prisma } from "@/database"
 import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
-
-// If your Prisma file is located elsewhere, you can change the path
+import { prisma } from "@/lib/prisma"
 
 export const auth = betterAuth({
   emailAndPassword: {
